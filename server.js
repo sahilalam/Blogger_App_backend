877203f684c8eea70c0a23710487dc00f9baa3ca
sahilalam <alamsahil939@gmail.com>
2021-04-15 21:13:52 +0530

    updated

--- server.js
+++ server.js
@@ -309,6 +309,7 @@ app.get('/get/blogs/:offset/:category/:from/:to/:myblogs',async(req,res)=>{
     }
     catch(err)
     {
+        console.log(err.message);
         res.status(500).json({
             message:err.message
         })
