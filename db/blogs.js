862849f8f2f9a052e3b1ad00d8bd4fa6736aaf33
sahilalam <alamsahil939@gmail.com>
2021-04-15 22:30:26 +0530

    updated

--- db/blogs.js
+++ db/blogs.js
@@ -10,7 +10,7 @@ const objectId=mongodb.ObjectId;
 let addBlog=async(title,category,subject,body,date,image_url,email,name)=>{
     try{
         date=new Date(date);
-       
+        
 
         const client=await mongoClient.connect(db_url);
         const db=await client.db(db_name);
@@ -45,10 +45,18 @@ let getBlogs=async(offset,filter)=>{
             if(filter.date)
             {
                let from =new Date(filter.date.from);
-            
+               from=from.getTime() 
+               from=new Date(from);
+               from.setHours(from.getHours() + 5); 
+               from.setMinutes(from.getMinutes() + 30);
+               from=new Date(from);
 
                let to=new Date(filter.date.to);
-              
+               to=to.getTime() 
+               to=new Date(to);
+               to.setHours(to.getHours() + 5); 
+               to.setMinutes(to.getMinutes() + 30);
+               to=new Date(to);
 
                 f.push({
                     'date':{$gte:from,$lte:to}
